3fbb9bb1315a3b67102701e8dfcd1a1b945e2a9a
Helen Fang <helenfang@DN0a25c472.SUNet>
2017-05-25 16:30:23 -0700

    Fixed paths

--- template.py
+++ template.py
@@ -9,21 +9,28 @@ import matplotlib.pyplot as plt
 import imutils
 import skimage
 
-templates = ['brown.png', 'purple.png', 'green.png', 'red.png', 'blue.png']
+templates = ['t0.jpg', 't1.jpg', 't2.jpg', 't3.jpg', 't4.jpg',
+			't5.jpg', 't6.jpg', 't7.jpg', 't8.jpg', 't9.jpg']
 methods = [
     'cv2.TM_CCOEFF',
     'cv2.TM_CCOEFF_NORMED',
     'cv2.TM_CCORR']
 
 def templateMatch(image, template):
-	sealion = cv2.imread('copy/' + template)
+	sealion = cv2.imread('templates/' + template)
 	for angle in [0, 45, 90]:
 		sealion_rot = imutils.rotate_bound(sealion, angle)
 		w, h = sealion_rot.shape[1], sealion_rot.shape[0]
 		sealion_noise = skimage.util.random_noise(sealion_rot, mode='gaussian')
+		
+		method = eval('cv2.TM_CCORR_NORMED')
+		res = cv2.matchTemplate(image, sealion_rot, method)
+		threshold = 0.8
+		loc = np.where( res >= threshold)
+		for pt in zip(*loc[::-1]):
+			cv2.rectangle(image,pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
 		'''
-		method = eval('cv2.TM_CCOEFF_NORMED')
-		res = cv2.matchTemplate(image, sealion, method)
+		res = cv2.matchTemplate(image, sealion_noise, method)
 		threshold = 0.8
 		loc = np.where( res >= threshold)
 		for pt in zip(*loc[::-1]):
